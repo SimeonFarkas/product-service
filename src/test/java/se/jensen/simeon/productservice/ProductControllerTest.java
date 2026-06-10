d93890115c1847afea47a5accc94ce44c261e9b0
SimeonFarkas <simeon.s.farkas@gmail.com>
2026-06-10 16:04:33 +0200

    Add WithMockUser to test

--- src/test/java/se/jensen/simeon/productservice/ProductControllerTest.java
+++ src/test/java/se/jensen/simeon/productservice/ProductControllerTest.java
@@ -3,6 +3,7 @@ package se.jensen.simeon.productservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import se.jensen.simeon.productservice.client.ProductClient;
@@ -26,6 +27,7 @@ public class ProductControllerTest {
     private ProductClient productClient;
 
     @Test
+    @WithMockUser
     void getProducts_shouldReturnList() throws Exception {
         List<Product> products = List.of(
                 new Product(1, "Test Product", 99.99, "Description", "category", "image.jpg", new Rating(4.0, 100))
